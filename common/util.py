3a882e3f56a7df4c98070a6d71d56275621fb79c
Hyde_ Caesar <lucifer7benjamin@sina.com>
2018-03-22 17:12:51 +0800

    提交分支
    修改完成

--- common/util.py
+++ common/util.py
@@ -132,15 +132,15 @@ def build_code_string_from_lex_tokens(tokens):
     """
     lex_tokens = iter(tokens)
     code_re = ""
-    lino_pre = 0
+    lino_pre = 1
     lexpos_pre = 0
     lexpos_temp = 0
     lenth_v = 0
     for token in lex_tokens:
         lino_temp = token.lineno
         if (lino_temp != lino_pre):
-            code_re = code_re + "\n"*(lino_temp-lino_pre)
-            lenth_v = lenth_v + 1
+            code_re = code_re + "\n"*(lino_temp - lino_pre)
+            lenth_v = lino_temp - lino_pre + 1
         else:
             code_re = code_re
         lino_pre = token.lineno
